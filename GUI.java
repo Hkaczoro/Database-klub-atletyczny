0d2c7d2458cdddd0ef07b33f0e61216b85dd9252
Jan Bogumił Janusz-Bielecki <j.janusz@stud.elka.pw.edu.pl>
2021-01-27 19:24:14 +0100

    Ukończenie całego CRUD-a

--- GUI.java
+++ GUI.java
@@ -13,13 +13,13 @@ import java.util.List;
 
 public class GUI extends JFrame implements ActionListener {
 
-    private JButton ButtonDisplay, Add, Save;
-    private JTextField Kategoria, Min_wiek, Max_wiek;
-    private JLabel kategoria, min_wiek, maks_wiek;
+    private JButton ButtonDisplay, Add, Save, Update, UpdateButton, Delete, DeleteButton;
+    private JTextField Kategoria, Min_wiek, Max_wiek, Nr_Kategorii, Nr_KategoriiD;
+    private JLabel kategoria, min_wiek, maks_wiek, nr_kategorii, nr_kategoriid;
     private JTable table;
     private JScrollPane sp;
     private Kategorie_wiekoweDAO dao;
-    private JPanel inputPanel;
+    private JPanel inputPanel, deletePanel, updatePanel;
     private DriverManagerDataSource dataSource;
     private DefaultTableModel model;
 
@@ -39,6 +39,16 @@ public class GUI extends JFrame implements ActionListener {
         add(Add);
         Add.addActionListener(this);
 
+        Update = new JButton("Update");
+        Update.setBounds(510,900,150,50);
+        add(Update);
+        Update.addActionListener(this);
+
+        Delete = new JButton("Delete");
+        Delete.setBounds(720,900,150,50);
+        add(Delete);
+        Delete.addActionListener(this);
+
         dataSource = new DriverManagerDataSource();
         dataSource.setUrl("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf");
         dataSource.setUsername("jjanusz");
@@ -49,9 +59,10 @@ public class GUI extends JFrame implements ActionListener {
 
         model = new javax.swing.table.DefaultTableModel(new String[] {"Nr_kategorii", "Kategoria", "Minimalny wiek", "Maksymalny wiek" },0);
         table = new JTable();
+        table.setSize(500,500);
         addDataToTable(dao,table,model);
         sp = new JScrollPane(table);
-        sp.setBounds(50,50,800,100);
+        sp.setBounds(50,50,800,200);
         add(sp);
         table.setEnabled(false);
         sp.setVisible(false);
@@ -66,7 +77,7 @@ public class GUI extends JFrame implements ActionListener {
         //pomocniczy panel do wprowadzania danych
         inputPanel = new JPanel();
         inputPanel.setLayout(new GridLayout(5, 4));
-        inputPanel.setBounds(500,500,250,90);
+        inputPanel.setBounds(500,500,400,110);
         inputPanel.add(kategoria);
         inputPanel.add(Kategoria);
         inputPanel.add(min_wiek);
@@ -80,8 +91,55 @@ public class GUI extends JFrame implements ActionListener {
         Save.addActionListener(this);
         this.add(inputPanel);
         inputPanel.setVisible(false);
+        //Tworzenie Delete Panelu
+        nr_kategoriid = new JLabel("Nr_Kategorii do usunięcia: ");
+        Nr_KategoriiD = new JTextField();
 
+        //pomocniczy panel do wprowadzania danych
+        deletePanel = new JPanel();
+        deletePanel.setLayout(new GridLayout(2, 2));
+        deletePanel.setBounds(500,500,400,50);
+        deletePanel.add(nr_kategoriid);
+        deletePanel.add(Nr_KategoriiD);
+        //tworzymy przycisk logowania
+        DeleteButton = new JButton("Delete");
+        DeleteButton.setBounds(100,470,400,80);
+        deletePanel.add(DeleteButton);
+        DeleteButton.addActionListener(this);
+        this.add(deletePanel);
+        deletePanel.setVisible(false);
 
+        //Tworzenie Update Panelu
+        nr_kategorii = new JLabel("Nr_Kategorii do usunięcia: ");
+        kategoria = new JLabel("Kategoria: ");
+        min_wiek = new JLabel("Minimalny wiek: ");
+        maks_wiek = new JLabel("Maksymalny wiek: ");
+        Kategoria = new JTextField();
+        Min_wiek = new JTextField();
+        Max_wiek = new JTextField();
+        Nr_Kategorii = new JTextField();
+
+        //pomocniczy panel do wprowadzania danych
+        updatePanel = new JPanel();
+        updatePanel.setLayout(new GridLayout(5, 5));
+        updatePanel.setBounds(500,500,400,110);
+        updatePanel.add(nr_kategorii);
+        updatePanel.add(Nr_Kategorii);
+        updatePanel.add(kategoria);
+        updatePanel.add(Kategoria);
+        updatePanel.add(min_wiek);
+        updatePanel.add(Min_wiek);
+        updatePanel.add(maks_wiek);
+        updatePanel.add(Max_wiek);
+        //tworzymy przycisk logowania
+        UpdateButton = new JButton("Update");
+        UpdateButton.setBounds(100,470,400,80);
+        updatePanel.add(UpdateButton);
+        UpdateButton.addActionListener(this);
+        this.add(updatePanel);
+        updatePanel.setVisible(false);
+
+        setResizable(false);
         setLayout(null);
 
     }
@@ -106,10 +164,17 @@ public class GUI extends JFrame implements ActionListener {
         if(source==ButtonDisplay){
             sp.setVisible(true);
             inputPanel.setVisible(false);
+            deletePanel.setVisible(false);
+            updatePanel.setVisible(false);
         }
 
         if(source==Add) {
-            sp.setVisible(false);
+            sp.setVisible(true);
+            deletePanel.setVisible(false);
+            updatePanel.setVisible(false);
+            Kategoria.setText("");
+            Min_wiek.setText("");
+            Max_wiek.setText("");
             inputPanel.setVisible(true);
 
         }
@@ -124,9 +189,60 @@ public class GUI extends JFrame implements ActionListener {
             Min_wiek.setText("");
             Max_wiek.setText("");
             inputPanel.setVisible(false);
+            deletePanel.setVisible(false);
+            updatePanel.setVisible(false);
             sp.setVisible(true);
         }
 
+        if(source==Delete){
+            inputPanel.setVisible(false);
+            updatePanel.setVisible(false);
+            deletePanel.setVisible(true);
+            sp.setVisible(true);
+            Nr_KategoriiD.setText("");
+        }
+
+        if(source==DeleteButton){
+            System.out.println("Delete");
+            //Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
+            int nr_kategorii = Integer.parseInt(Nr_KategoriiD.getText());
+            System.out.println(Nr_KategoriiD.getText());
+            dao.delete(nr_kategorii);
+            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+            model.getDataVector().removeAllElements();
+            addDataToTable(dao,table,model);
+            Nr_KategoriiD.setText("");
+
+
+        }
+
+        if(source==Update){
+            inputPanel.setVisible(false);
+            deletePanel.setVisible(false);
+            updatePanel.setVisible(true);
+            sp.setVisible(true);
+            Nr_Kategorii.setText("");
+            Kategoria.setText("");
+            Min_wiek.setText("");
+            Max_wiek.setText("");
+        }
+
+        if (source==UpdateButton){
+            System.out.println("Update");
+            Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
+            kategorie_wiekowe.setNr_kategorii(Integer.parseInt(Nr_Kategorii.getText()));
+            kategorie_wiekowe.setKategoria(Kategoria.getText());
+            kategorie_wiekowe.setMinimalny_wiek(Integer.parseInt(Min_wiek.getText()));
+            kategorie_wiekowe.setMaksymalny_wiek(Integer.parseInt(Max_wiek.getText()));
+            dao.update(kategorie_wiekowe);
+            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+            model.getDataVector().removeAllElements();
+            addDataToTable(dao,table,model);
+            Nr_Kategorii.setText("");
+            Kategoria.setText("");
+            Min_wiek.setText("");
+            Max_wiek.setText("");
+        }
 
     }
 
