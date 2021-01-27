f6e6bd940453f2e565de78eb98f98507fa53dfff
Jan Bogumił Janusz-Bielecki <j.janusz@stud.elka.pw.edu.pl>
2021-01-27 21:49:08 +0100

    Dodanie opcji wyświetlania innych tabel

--- GUI.java
+++ GUI.java
@@ -4,6 +4,7 @@ package Example.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.swing.*;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -23,6 +24,7 @@ public class GUI extends JFrame implements ActionListener {
     private JPanel inputPanel, deletePanel, updatePanel;
     private DriverManagerDataSource dataSource;
     private DefaultTableModel model;
+    private JComboBox changeTable;
 
     public GUI() {
         super("Klub lekkoatletyczny");
@@ -68,13 +70,21 @@ public class GUI extends JFrame implements ActionListener {
         dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
         daozaw = new ZawodnicyDAO(new JdbcTemplate(dataSource));
 
+        String[] tables = {"Kategorie wiekowe", "Zawodnicy", "Trenerzy"};
+        changeTable = new JComboBox(tables);
+        changeTable.setBounds(50, 20, 200, 30);
+        changeTable.setSelectedIndex(0);
+        add(changeTable);
+        changeTable.addActionListener(this);
+
 
         model = new javax.swing.table.DefaultTableModel(new String[] {"Nr_kategorii", "Kategoria", "Minimalny wiek", "Maksymalny wiek" },0);
         table = new JTable();
         table.setSize(500,500);
         addDataToTable(dao,table,model);
         sp = new JScrollPane(table);
-        sp.setBounds(50,50,800,200);
+        sp.setBounds(50,50,800,300);
+        table.setBackground(Color.WHITE);
         add(sp);
         table.setEnabled(false);
         sp.setVisible(false);
@@ -169,120 +179,170 @@ public class GUI extends JFrame implements ActionListener {
 
     }
 
+    public void addDataZawToTable(ZawodnicyDAO daozaw, JTable table, DefaultTableModel model) {
+        List<String> listZ1 =daozaw.listcol("NR_ZAWODNIKA");
+        List<String> listZ2 =daozaw.listcol("IMIE");
+        List<String> listZ3 =daozaw.listcol("NAZWISKO");
+        List<String> listZ4 =daozaw.listcol("PLEC");
+        List<String> listZ5 =daozaw.listcol("WZROST");
+        List<String> listZ6 =daozaw.listcol("WAGA");
+        List<String> listZ7 =daozaw.listcol("DATA_URODZENIA");
+        List<String> listZ8 =daozaw.listcol("NARODOWOSC");
+        List<String> listZ9 =daozaw.listcol("PESEL");
+        List<String> listZ10 =daozaw.listcol("NR_KATEGORII");
+        List<String> listZ11 =daozaw.listcol("NR_ADRESU");
+        System.out.println(listZ1);
+        table.setModel(model);
+        for(int i=0;i<listZ1.size();i++) {
+            model.addRow(new Object[] {listZ1.get(i),listZ2.get(i),listZ3.get(i),listZ4.get(i),listZ5.get(i),listZ6.get(i),listZ7.get(i),listZ8.get(i),listZ9.get(i),listZ10.get(i),listZ11.get(i),});
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
 
-        if(source==ButtonDisplay){
-            sp.setVisible(true);
-            inputPanel.setVisible(false);
-            deletePanel.setVisible(false);
-            updatePanel.setVisible(false);
-        }
-
-        if(source==Add) {
-            sp.setVisible(true);
-            deletePanel.setVisible(false);
-            updatePanel.setVisible(false);
-            Kategoria.setText("");
-            Min_wiek.setText("");
-            Max_wiek.setText("");
-            inputPanel.setVisible(true);
-
-        }
-
-        if(source==Save){
-            Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe(0,Kategoria.getText(),Integer.parseInt(Min_wiek.getText()),Integer.parseInt(Max_wiek.getText()));
-            dao.save(kategorie_wiekowe);
+        if (changeTable.getSelectedIndex() == 0){
             dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
             model.getDataVector().removeAllElements();
             addDataToTable(dao,table,model);
-            Kategoria.setText("");
-            Min_wiek.setText("");
-            Max_wiek.setText("");
-            inputPanel.setVisible(false);
-            deletePanel.setVisible(false);
-            updatePanel.setVisible(false);
-            sp.setVisible(true);
-        }
+            if(source==ButtonDisplay){
+                sp.setVisible(true);
+                inputPanel.setVisible(false);
+                deletePanel.setVisible(false);
+                updatePanel.setVisible(false);
+            }
 
-        if(source==Delete){
-            inputPanel.setVisible(false);
-            updatePanel.setVisible(false);
-            deletePanel.setVisible(true);
-            sp.setVisible(true);
-            Nr_KategoriiD.setText("");
-        }
+            if(source==Add) {
+                sp.setVisible(true);
+                deletePanel.setVisible(false);
+                updatePanel.setVisible(false);
+                Kategoria.setText("");
+                Min_wiek.setText("");
+                Max_wiek.setText("");
+                inputPanel.setVisible(true);
 
-        if(source==DeleteButton){
-            System.out.println("Delete");
-            //Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
-            int nr_kategorii = Integer.parseInt(Nr_KategoriiD.getText());
-            System.out.println(Nr_KategoriiD.getText());
-            dao.delete(nr_kategorii);
-            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
-            model.getDataVector().removeAllElements();
-            addDataToTable(dao,table,model);
-            Nr_KategoriiD.setText("");
+            }
+
+            if(source==Save){
+                Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe(0,Kategoria.getText(),Integer.parseInt(Min_wiek.getText()),Integer.parseInt(Max_wiek.getText()));
+                dao.save(kategorie_wiekowe);
+                dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+                model.getDataVector().removeAllElements();
+                addDataToTable(dao,table,model);
+                Kategoria.setText("");
+                Min_wiek.setText("");
+                Max_wiek.setText("");
+                inputPanel.setVisible(false);
+                deletePanel.setVisible(false);
+                updatePanel.setVisible(false);
+                sp.setVisible(true);
+            }
+
+            if(source==Delete){
+                inputPanel.setVisible(false);
+                updatePanel.setVisible(false);
+                deletePanel.setVisible(true);
+                sp.setVisible(true);
+                Nr_KategoriiD.setText("");
+            }
+
+            if(source==DeleteButton){
+                System.out.println("Delete");
+                //Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
+                int nr_kategorii = Integer.parseInt(Nr_KategoriiD.getText());
+                System.out.println(Nr_KategoriiD.getText());
+                dao.delete(nr_kategorii);
+                dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+                model.getDataVector().removeAllElements();
+                addDataToTable(dao,table,model);
+                Nr_KategoriiD.setText("");
 
 
-        }
+            }
 
-        if(source==Update){
-            inputPanel.setVisible(false);
-            deletePanel.setVisible(false);
-            updatePanel.setVisible(true);
-            sp.setVisible(true);
-            Nr_Kategorii.setText("");
-            KategoriaU.setText("");
-            Min_wiekU.setText("");
-            Max_wiekU.setText("");
-        }
+            if(source==Update){
+                inputPanel.setVisible(false);
+                deletePanel.setVisible(false);
+                updatePanel.setVisible(true);
+                sp.setVisible(true);
+                Nr_Kategorii.setText("");
+                KategoriaU.setText("");
+                Min_wiekU.setText("");
+                Max_wiekU.setText("");
+            }
 
-        if (source==UpdateButton){
-            System.out.println("Update");
-            Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
-            kategorie_wiekowe.setNr_kategorii(Integer.parseInt(Nr_Kategorii.getText()));
-            kategorie_wiekowe.setKategoria(KategoriaU.getText());
-            kategorie_wiekowe.setMinimalny_wiek(Integer.parseInt(Min_wiekU.getText()));
-            kategorie_wiekowe.setMaksymalny_wiek(Integer.parseInt(Max_wiekU.getText()));
-            dao.update(kategorie_wiekowe);
-            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
-            model.getDataVector().removeAllElements();
-            addDataToTable(dao,table,model);
-            Nr_Kategorii.setText("");
-            KategoriaU.setText("");
-            Min_wiekU.setText("");
-            Max_wiekU.setText("");
-        }
+            if (source==UpdateButton){
+                System.out.println("Update");
+                Kategorie_wiekowe kategorie_wiekowe = new Kategorie_wiekowe();
+                kategorie_wiekowe.setNr_kategorii(Integer.parseInt(Nr_Kategorii.getText()));
+                kategorie_wiekowe.setKategoria(KategoriaU.getText());
+                kategorie_wiekowe.setMinimalny_wiek(Integer.parseInt(Min_wiekU.getText()));
+                kategorie_wiekowe.setMaksymalny_wiek(Integer.parseInt(Max_wiekU.getText()));
+                dao.update(kategorie_wiekowe);
+                dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+                model.getDataVector().removeAllElements();
+                addDataToTable(dao,table,model);
+                Nr_Kategorii.setText("");
+                KategoriaU.setText("");
+                Min_wiekU.setText("");
+                Max_wiekU.setText("");
+            }
 
-        if(source==Refresh){
-            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
-            model.getDataVector().removeAllElements();
-            addDataToTable(dao,table,model);
-        }
+            if(source==Refresh){
+                dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+                model.getDataVector().removeAllElements();
+                addDataToTable(dao,table,model);
+            }
 
-        if(source==DeleteAll){
-            List<String> listD1 =dao.listcol("NR_KATEGORII");
-            List<String> listD2 =daozaw.listcol("NR_KATEGORII");
-            table.setModel(model);
-            for(int i=0;i<listD1.size();i++) {
-                for(int j=0;j<listD2.size();j++){
-                    if(!listD1.get(i).equals(listD2.get(j))){
-                        dao.delete(Integer.parseInt(listD1.get(i)));
-                        j=listD2.size();
+            if(source==DeleteAll){
+                List<String> listD1 =dao.listcol("NR_KATEGORII");
+                List<String> listD2 =daozaw.listcol("NR_KATEGORII");
+                table.setModel(model);
+                for(int i=0;i<listD1.size();i++) {
+                    for(int j=0;j<listD2.size();j++){
+                        if(!listD1.get(i).equals(listD2.get(j))){
+                            dao.delete(Integer.parseInt(listD1.get(i)));
+                            j=listD2.size();
+                        }
                     }
                 }
+                dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
+                model.getDataVector().removeAllElements();
+                addDataToTable(dao,table,model);
             }
-            dao = new Kategorie_wiekoweDAO(new JdbcTemplate(dataSource));
-            model.getDataVector().removeAllElements();
-            addDataToTable(dao,table,model);
+
         }
+        if (changeTable.getSelectedIndex() == 1){
+            sp.setSize(900,500);
+            table.setSize(900,500);
+            model.getDataVector().removeAllElements();
+            model.setColumnIdentifiers(new String[] {"Nr_zawodnika", "Imie", "Nazwisko", "Płeć", "Wzrost", "Waga", "Data_urodzenia", "Narodowość", "Pesel", "Nr_kategorii", "Nr_adresu"});
+            table.getColumnModel().getColumn(2).setPreferredWidth(100);
+            table.getColumnModel().getColumn(6).setPreferredWidth(130);
+            table.getColumnModel().getColumn(8).setPreferredWidth(100);
+            daozaw = new ZawodnicyDAO(new JdbcTemplate(dataSource));
+            addDataZawToTable(daozaw,table,model);
+            if(source==ButtonDisplay){
+                sp.setVisible(true);
+                inputPanel.setVisible(false);
+                deletePanel.setVisible(false);
+                updatePanel.setVisible(false);
+            }
+
+        }
+        if (changeTable.getSelectedIndex() == 2){
+
+        }
+
 
     }
 
 
-
-
-
 }
+
+
+
+
+
+
